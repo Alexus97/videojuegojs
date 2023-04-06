600e4a9b20c9fd05ebf83a21bcfe5397b3fd498f
jonathan <lexusjt98@gmail.com>
2023-04-06 00:27:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1'); //traemos el elemento
+    const ctx = canvas.getContext('2d'); //En que dimension vamos a pintar en el lienzo
+    //definimos los pixeles de el lienzo width y height
+    canvas.width = 1280;
+    canvas.height = 720;
+    
+})
